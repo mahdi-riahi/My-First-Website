b75b0ec20f7cfc052e763d88e85cba8e7ce39f92
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-08-31 17:16:00 +0330

    I designed a simple template (http://localhost/blog/first/): I made a directory in the blog app for templates (blog/Templates/blog) And I wrote a html file in there to be shown in the address. I created a new view(func) for this template(blog/views). And I added a new path for it in blog/urls (/first).

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def welcome_blog(request):
-    return HttpResponse('<h1 style="background-color:blue; color; white;">Hello\nWellcome to my blog</h1><p>This is our first page</p>')
+    return HttpResponse('<h1 style="background-color:blue; color; white;">Hello\nWellcome to my blog</h1><p>This is our first page made with simple html,css</p><br><a href="first">First article</a>')
+
+def first_article(request):
+    context={'currentdatetime':datetime.now().strftime("%Y-%m-%d  %H:%M:%S")}
+    return render(request,'blog/index.html',context)
