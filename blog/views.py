cabcd23aed7d08fba45ce4fd48ae2a92f026b825
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-08-31 11:19:48 +0330

    blog app paths added to project : blog/views.py edited , blog/urls.py added and edited , mysite/urls.py edited

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def welcome_blog(request):
+    return HttpResponse('<h1 style="background-color:blue; color; white;">Hello\nWellcome to my blog</h1><p>This is our first page</p>')
